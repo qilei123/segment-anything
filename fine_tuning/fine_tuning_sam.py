a42c434e0a6ba936415c08d5d255b3b61e5a6b76
qileimail123 <qileimail123@gmail.com>
2023-04-21 02:55:12 -0400

    prepare for fine tuning

---
+++ fine_tuning/fine_tuning_sam.py
@@ -0,0 +1,164 @@
+from preprocess import *
+
+model_type = 'vit_b'
+checkpoint = 'sam_vit_b_01ec64.pth'
+device = 'cuda:0'
+
+from segment_anything import SamPredictor, sam_model_registry
+
+sam_model = sam_model_registry[model_type](checkpoint=checkpoint)
+sam_model.to(device)
+sam_model.train()
+
+# Preprocess the images
+from collections import defaultdict
+
+import torch
+
+from segment_anything.utils.transforms import ResizeLongestSide
+
+transformed_data = defaultdict(dict)
+for k in bbox_coords.keys():
+    image = cv2.imread(f'scans/scans/{k}.png')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    transform = ResizeLongestSide(sam_model.image_encoder.img_size)
+    input_image = transform.apply_image(image)
+    input_image_torch = torch.as_tensor(input_image, device=device)
+    transformed_image = input_image_torch.permute(
+        2, 0, 1).contiguous()[None, :, :, :]
+
+    input_image = sam_model.preprocess(transformed_image)
+    original_image_size = image.shape[:2]
+    input_size = tuple(transformed_image.shape[-2:])
+
+    transformed_data[k]['image'] = input_image
+    transformed_data[k]['input_size'] = input_size
+    transformed_data[k]['original_image_size'] = original_image_size
+
+# Set up the optimizer, hyperparameter tuning will improve performance here
+lr = 1e-4
+wd = 0
+optimizer = torch.optim.Adam(sam_model.mask_decoder.parameters(),
+                             lr=lr,
+                             weight_decay=wd)
+
+loss_fn = torch.nn.MSELoss()
+# loss_fn = torch.nn.BCELoss()
+keys = list(bbox_coords.keys())
+
+from statistics import mean
+
+from tqdm import tqdm
+from torch.nn.functional import threshold, normalize
+
+num_epochs = 100
+losses = []
+
+for epoch in range(num_epochs):
+    epoch_losses = []
+    # Just train on the first 20 examples
+    for k in keys[:20]:
+        input_image = transformed_data[k]['image'].to(device)
+        input_size = transformed_data[k]['input_size']
+        original_image_size = transformed_data[k]['original_image_size']
+
+        # No grad here as we don't want to optimise the encoders
+        with torch.no_grad():
+            image_embedding = sam_model.image_encoder(input_image)
+
+            prompt_box = bbox_coords[k]
+            box = transform.apply_boxes(prompt_box, original_image_size)
+            box_torch = torch.as_tensor(box, dtype=torch.float, device=device)
+            box_torch = box_torch[None, :]
+
+            sparse_embeddings, dense_embeddings = sam_model.prompt_encoder(
+                points=None,
+                boxes=box_torch,
+                masks=None,
+            )
+        low_res_masks, iou_predictions = sam_model.mask_decoder(
+            image_embeddings=image_embedding,
+            image_pe=sam_model.prompt_encoder.get_dense_pe(),
+            sparse_prompt_embeddings=sparse_embeddings,
+            dense_prompt_embeddings=dense_embeddings,
+            multimask_output=False,
+        )
+
+        upscaled_masks = sam_model.postprocess_masks(
+            low_res_masks, input_size, original_image_size).to(device)
+        binary_mask = normalize(threshold(upscaled_masks, 0.0, 0))
+
+        gt_mask_resized = torch.from_numpy(
+            np.resize(ground_truth_masks[k],
+                      (1, 1, ground_truth_masks[k].shape[0],
+                       ground_truth_masks[k].shape[1]))).to(device)
+        gt_binary_mask = torch.as_tensor(gt_mask_resized > 0,
+                                         dtype=torch.float32)
+
+        loss = loss_fn(binary_mask, gt_binary_mask)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        epoch_losses.append(loss.item())
+    losses.append(epoch_losses)
+    print(f'EPOCH: {epoch}')
+    print(f'Mean loss: {mean(epoch_losses)}')
+
+mean_losses = [mean(x) for x in losses]
+mean_losses
+
+plt.plot(list(range(len(mean_losses))), mean_losses)
+plt.title('Mean epoch loss')
+plt.xlabel('Epoch Number')
+plt.ylabel('Loss')
+plt.savefig("loss.png")
+plt.show()
+
+# Load up the model with default weights
+sam_model_orig = sam_model_registry[model_type](checkpoint=checkpoint)
+sam_model_orig.to(device)
+
+# Set up predictors for both tuned and original models
+from segment_anything import sam_model_registry, SamPredictor
+
+predictor_tuned = SamPredictor(sam_model)
+predictor_original = SamPredictor(sam_model_orig)
+
+# The model has not seen keys[21] (or keys[20]) since we only trained on keys[:20]
+k = keys[21]
+image = cv2.imread(f'scans/scans/{k}.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+predictor_tuned.set_image(image)
+predictor_original.set_image(image)
+
+input_bbox = np.array(bbox_coords[k])
+
+masks_tuned, _, _ = predictor_tuned.predict(
+    point_coords=None,
+    box=input_bbox,
+    multimask_output=False,
+)
+
+masks_orig, _, _ = predictor_original.predict(
+    point_coords=None,
+    box=input_bbox,
+    multimask_output=False,
+)
+
+#%matplotlib inline
+_, axs = plt.subplots(1, 2, figsize=(25, 25))
+
+axs[0].imshow(image)
+show_mask(masks_tuned, axs[0])
+show_box(input_bbox, axs[0])
+axs[0].set_title('Mask with Tuned Model', fontsize=26)
+axs[0].axis('off')
+
+axs[1].imshow(image)
+show_mask(masks_orig, axs[1])
+show_box(input_bbox, axs[1])
+axs[1].set_title('Mask with Untuned Model', fontsize=26)
+axs[1].axis('off')
+
+plt.show()
---
+++ fine_tuning/preprocess.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import glob
+from PIL import Image
+import os
+
+
+data_root = 'TN-SCUI2020/segmentation'
+
+img_list = sorted(glob.glob(os.path.join(data_root,"image/*.bmp")))
+msk_list = sorted(glob.glob(os.path.join(data_root,"mask/*.bmp")))
+
+train_size = 500*2
+
+bbox_coords = {}
+ground_truth_masks = {}
+
+for index,img_dir,msk_dir in enumerate(zip(img_list[:train_size],msk_list[:train_size])):
+    
+    gray_image = np.array(Image.open(img_dir))
+    
+    mask = np.array(Image.open(msk_dir))
+    
+    ground_truth_masks[index] = mask
+    
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST,
+                                            cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+    x, y, w, h = cv2.boundingRect(contours[0]) #第一个为最大连通域，为目标区域
+    height, width, _ = gray_image.shape
+    bbox_coords[index] = np.array([x, y, x + w, y + h])
+
+
+# Helper functions provided in https://github.com/facebookresearch/segment-anything/blob/9e8f1309c94f1128a6e5c047a10fdcb02fc8d651/notebooks/predictor_example.ipynb
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+    
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
+'''    
+name = 'stampDS-00004'
+image = cv2.imread(f'scans/scans/{name}.png')
+
+plt.figure(figsize=(10,10))
+plt.imshow(image)
+show_box(bbox_coords[name], plt.gca())
+show_mask(ground_truth_masks[name], plt.gca())
+plt.axis('off')
+plt.show()
+plt.savefig(f"test.jpg")
+'''
